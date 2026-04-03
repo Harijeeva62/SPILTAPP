a16633f9136e303a0638905c2ab85cd965f25735
hari J <harijeeva62@gmail.com>
2026-04-03 18:48:56 +0530

    Delete server directory

--- server/src/config/supabase.js
+++
@@ -1,6 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const config = require('./index');
-
-const supabase = createClient(config.supabaseUrl, config.supabaseServiceKey);
-
-module.exports = supabase;
